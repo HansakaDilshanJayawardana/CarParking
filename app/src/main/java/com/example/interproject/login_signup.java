dd3f97f22e9bddf89f58144e082752e55badde15
HansakaDilshanJayawardana <hansaka345@gmail.com>
2021-06-24 21:29:38 +0530

    Car Parking UIs

---
+++ app/src/main/java/com/example/interproject/login_signup.java
@@ -0,0 +1,14 @@
+package com.example.interproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup);
+    }
+}
